1e23200104192c761bddc0e0c092b5fdd08fbacd
Guizhi <99305855+EEGuizhi@users.noreply.github.com>
2024-06-02 16:03:37 +0800

    update DIP final project

--- Image_Processing/1112_Final_Project/Final_Project.py
+++ Image_Processing/1112_Final_Project/Final_Project.py
@@ -1,6 +1,7 @@
 # 4109061012 B.S.Chen
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 """
 Python implementation code of:
@@ -8,7 +9,7 @@ Pouli, Tania, and Erik Reinhard. "Progressive color transfer for images of arbit
 - By B.S.Chen National Chung Hsing University
 """
 
-# Parameters
+# Settings
 PATH_SRC = "Image_Processing/1112_Final_Project/source/source_01.jpg"
 PATH_TAR = "Image_Processing/1112_Final_Project/target/target_01.jpg"
 
@@ -61,8 +62,9 @@ def resample_histogram(Hist: np.ndarray, Bins: int, normalize: bool) -> np.ndarr
     return resampHist
 
 
-def ReshapeHistogram(Is: np.ndarray, It: np.ndarray, perc: int, Wa: float=1.0) -> np.ndarray:
-    """
+def ReshapeHistogram(Is: np.ndarray, It: np.ndarray, perc: int, Wa: float=1.0, show_hist=False) -> np.ndarray:
+    """ Reshape the histogram of `Is` to be similar as `It`
+
     Parameters:
     ---
         `Is`: Source image
@@ -93,23 +95,25 @@ def ReshapeHistogram(Is: np.ndarray, It: np.ndarray, perc: int, Wa: float=1.0) -
             threshold = Wa * (CH_RANGE[ic][1] - CH_RANGE[ic][0] + 1)
             Mask[source_lab[:, :, ic] > threshold] = 1
 
-        for k in range(0, round((perc/100) * Smax[ic])):
+        for k in range(1, round((perc/100) * Smax[ic]) + 1):
             # Compute Bk
             Bk = int(Bins[ic] * pow(2, k - Smax[ic]))
-            print(f"\n>> k: {k}, Bk: {Bk}")
+            print(f">> k: {k}, Bk: {Bk},   ", end='')
 
             # Down-sample and then up-sample
+            print("resampling..  ", end='')
             Hs_k, Ht_k = resample_histogram(Hs, Bk, True), resample_histogram(Ht, Bk, True)
             Hs_k, Ht_k = resample_histogram(Hs_k, Bins[ic], True), resample_histogram(Ht_k, Bins[ic], True)
 
             # Region transfer 1
+            print("region transfering.. ")
             Rmin_t = findpeaks(Ht_k)
             Hs_kp = np.empty_like(Hs_k)
             for m in range(len(Rmin_t) - 1):
                 Hs_kp[Rmin_t[m]:Rmin_t[m+1]] = RegionTransfer(
                     Hs_k[Rmin_t[m]:Rmin_t[m+1]],
                     Ht_k[Rmin_t[m]:Rmin_t[m+1]],
-                    (k+1) / Smax[ic]
+                    k / Smax[ic]
                 )
 
             # Region transfer 2
@@ -119,15 +123,15 @@ def ReshapeHistogram(Is: np.ndarray, It: np.ndarray, perc: int, Wa: float=1.0) -
                 Ho_k[Rmin_s[m]:Rmin_s[m+1]] = RegionTransfer(
                     Hs_k[Rmin_s[m]:Rmin_s[m+1]],
                     Ht_k[Rmin_s[m]:Rmin_s[m+1]],
-                    (k+1) / Smax[ic]
+                    k / Smax[ic]
                 )
 
             # Output becomes next round's input
             Hs = Ho_k
 
+        print(">> histogram matching.. \n")
         Hs = histogram(source_lab[:, :, ic], CH_RANGE[ic], False)
-        print(f"\n>> HistMatch: ic = {ic}")
-        Io[:, :, ic] = HistMatch(
+        Io[:, :, ic] = histogram_matching(
             source_lab[:, :, ic],
             Hs,
             Ho_k,
@@ -136,7 +140,17 @@ def ReshapeHistogram(Is: np.ndarray, It: np.ndarray, perc: int, Wa: float=1.0) -
             Mask
         )
 
+        # Plot histograms
+        if show_hist:
+            plt.plot(Hs / Hs.sum(), label='source')
+            plt.plot(Ht / Ht.sum(), label='target')
+            plt.plot(Ho_k, label='output')
+            plt.xlim(left=0, right=256), plt.ylim(top=0.1, bottom=0), plt.legend()
+            plt.show(), cv2.waitKey(0)
+
+    # Convert Io back to RGB color space
     output_img = cv2.cvtColor(Io, cv2.COLOR_Lab2BGR)
+    print(">> done")
     return output_img
 
 
@@ -165,41 +179,41 @@ def grad(arr: np.ndarray) -> np.ndarray:
 
 
 def RegionTransfer(Hs: np.ndarray, Ht: np.ndarray, wt: float) -> np.ndarray:
-    """ Region transfer function using adjusted Eq.10~12 """
+    """ Region transfer function using adjusted Eqs.10~12 """
+    ws = 1 - wt
     Hs_avg, Ht_avg = Hs.mean(), Ht.mean()
     Hs_std, Ht_std = Hs.std(), Ht.std()
-    print(f">> avgs / stds: {Hs_avg}, {Ht_avg} / {Hs_std}, {Ht_std}")
 
     Ho = Hs.copy()
-    for i in range(Ho.shape[0]):
-        if round(Hs_std, 6) != 0:
-            Ho[i] = (Ho[i] - Hs_avg) * (wt*Ht_std + (1-wt)*Hs_std) / Hs_std + wt * Ht_avg + (1-wt) * Hs_avg
-        else:
-            Ho[i] = Ho[i] - Hs_avg + wt * Ht_avg + (1-wt) * Hs_avg
+    if round(Hs_std, 6) != 0:
+        Ho = (Ho - Hs_avg) * (wt * Ht_std + ws * Hs_std) / Hs_std + wt * Ht_avg + ws * Hs_avg
+    else:
+        Ho = Ho - Hs_avg + wt * Ht_avg + ws * Hs_avg
 
     return Ho
 
 
-def HistMatch(Is: np.ndarray, Hs: np.ndarray, Ho: np.ndarray, channel: int, pixel_count: int, Im: np.ndarray) -> np.ndarray:
+def histogram_matching(Is: np.ndarray, Hs: np.ndarray, Ho: np.ndarray, channel: int, pixel_count: int, Im: np.ndarray) -> np.ndarray:
+    """ Performing histogram matching """
     # Initialize
     Imin, Imax = CH_RANGE[channel][0], CH_RANGE[channel][1]
-    Hs = Hs / Hs.sum() * pixel_count
-    Ho = Ho / Ho.sum() * pixel_count
+    Hs_sum, Ho_sum = Hs.sum(), Ho.sum()
+    Hs = Hs * pixel_count / Hs_sum
+    Ho = Ho * pixel_count / Ho_sum
 
     # Cumulative Histogram
     Ho = np.cumsum(Ho)
     Hs = np.cumsum(Hs)
-    print(f">> Ho length = {Ho.shape[0]}, Hs length = {Hs.shape[0]}")
 
     # Below line: second & third params are "x-coord" and "y-coord corresponding to x-coord",
     # the first param is sample x-coords, which is used to get the corresponding y-coords.
     new_pix_value = np.interp(Hs, Ho, np.arange(Imin, Imax))
-    Io = Is.ravel() - Imin  # flatten image
-    Im = Im.ravel()  # flatten mask
 
+    Io, Im = Is.ravel(), Im.ravel()
     Io[Im == 0] = new_pix_value[Io[Im == 0]]
     Io = np.reshape(Io, Is.shape)
-    return np.uint8(Io)
+
+    return Io.astype(dtype=np.uint8)
 
 
 if __name__ == "__main__":
@@ -208,5 +222,7 @@ if __name__ == "__main__":
     target_img = np.asarray(cv2.imread(PATH_TAR, cv2.IMREAD_COLOR), dtype=np.uint8)
 
     for perc in PERC:
-        output_img = ReshapeHistogram(source_img, target_img, perc, W_A)
+        print("\n=======================================================")
+        print(f">> Start the case of perc = {perc}%")
+        output_img = ReshapeHistogram(source_img, target_img, perc, W_A, False)
         cv2.imwrite(f"Image_Processing/1112_Final_Project/output/ColorTransfer_perc{perc}.png", output_img)
